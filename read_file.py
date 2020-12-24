19c2da925df86661a093feb1d7809369cbc66168
Rafaela Sousa <rafaelaprm@gmail.com>
2020-12-23 22:11:02 -0400

    [update] Read the file and insert data into database

--- read_file.py
+++ read_file.py
@@ -1,10 +1,17 @@
 import csv
+from pessoa_connection import insere_pessoa, retorna_pessoas
 
 def read_file():
     with open('files/curso-mvcad.csv', "r", encoding="utf8") as file:
         reader = csv.DictReader(file, delimiter=',')
     
-        list_people = [item for item in reader]
-        print(list_people[0])
+        #list_people = [item for item in reader]
+        #print(list_people[0])
+
+        for pessoa in reader:
+            cpf = pessoa['cpf'].replace('.', '')
+            pessoa['cpf'] = cpf.replace('-', '')
+            insere_pessoa(pessoa)
+
 
 read_file()
